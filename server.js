03911660cada09b6ffd794471bfbaf4da78b51d8
Dot-Rhs <rhysh1986@gmail.com>
2023-08-29 23:16:42 +0100

    Heroku patching

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const dbConnect = require("./public/js/db/dbConnect");
 const UpdateCount = require("./public/js/models/count");
 
 app.engine("html", require("ejs").renderFile);
-app.listen(5200, () => console.log("Connected to sordid little grief hole."));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Connected to sordid little grief hole."),
+);
 
 app.get("/", async (req, res) => {
   if (res.statusCode === 200) {
